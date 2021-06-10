05c7e798eee0cef61ca1450c7f33771ea5f251c3
kadamharsh <76954925+kadamharsh@users.noreply.github.com>
2021-06-10 07:49:46 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+var ball;
+var ballposition;
+var database;
+var position;
+function setup(){
+    database=firebase.database()
+    createCanvas(500,500);
+    ball = createSprite(262,374,10,10);
+    ball.shapeColor = "red";
+    ballposition=database.ref('ball/position');
+    ballposition.on("value",readPosition)
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+database.ref('ball/position').set({
+    'x':position.  x + x,
+    'y':position.  y + y
+})
+
+
+
+
+
+
+
+}
+
+
+
+function readPosition(data)
+{
+    position=data.val();
+    ball.x=position.x
+    ball.y=position.y
+}
+
+
+
+
+
+
+
+
+
+
+
+
